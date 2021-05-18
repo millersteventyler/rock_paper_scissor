f29bbebe0d1ee06014d766e492baba53a54c1fdd
millersteventyler <millersteventyler@gmail.com>
2021-05-18 13:39:22 -0400

    Removed method from human.py becuase parent class has same method.

--- human.py
+++ human.py
@@ -3,8 +3,4 @@ from player import Player
 
 class Human(Player):
     def __init__(self):
-        pass
-        super().__init__(name='')
-
-    def get_gesture_with_input(self):
-        pass
+        super().__init__('')
