944030332f8fe54e09cbca32416fb460802dae74
millersteventyler <millersteventyler@gmail.com>
2021-05-19 16:06:42 -0400

    Cleaned up method choosing_gesture in order to simplify my code.

--- ai.py
+++ ai.py
@@ -12,20 +12,6 @@ class Ai(Player):
 
     def choosing_gesture(self):
         user_input = random.randint(0, len(self.gestures) - 1)
-        if user_input == 0:
-            user_input = self.gestures[0]
-            print(f'{self.name} used Rock')
-        elif user_input == 1:
-            user_input = self.gestures[1]
-            print(f'{self.name} used Paper')
-        elif user_input == 2:
-            user_input = self.gestures[2]
-            print(f'{self.name} used Scissors')
-        elif user_input == 3:
-            user_input = self.gestures[3]
-            print(f'{self.name} used Lizard')
-        elif user_input == 4:
-            user_input = self.gestures[4]
-            print(f'{self.name} used Spock')
-        self.chosen_gesture = user_input
+        self.chosen_gesture = self.gestures[user_input]
+        print(f'{self.name} used {self.chosen_gesture}')
         return self.chosen_gesture
